b76f77ffb85ff529c001d03dd431a5c8bf461e04
Ume Tabassum <surazeeshan@gmail.com>
2026-04-18 15:46:23 +0500

    create page and route

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import React from "react";
 
-const App = () => {
-  return (
-    <div className="flex items-center justify-center h-screen">
-      <h1 className="text-red-600 ">Hello, World!</h1>
-    </div>
-  );
-};
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Profile from './pages/Profile';
 
-export default App;
+export default function App() {
+  return <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/sign-in' element={<SignIn />} />
+      <Route path='/sign-up' element={<SignUp />} />
+      <Route path='/profile' element={<Profile />} />
+    </Routes>
+  </BrowserRouter>
+    
+  
+}
+
