5d1fdd7c7855e5d45af90cc176e7f4cbbca52d6e
Ume Tabassum <surazeeshan@gmail.com>
2026-04-18 13:04:44 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-red-600 ">Hello, World!</h1>
+    </div>
+  );
+};
+
+export default App;
